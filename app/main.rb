923c2d2a18dff218078978eb4cccf2b00f27d7f2
Brett Chalupa <brettchalupa@gmail.com>
2022-11-26 19:25:38 -0500

    Initial commit

---
+++ app/main.rb
@@ -0,0 +1,27 @@
+TRUE_BLACK = [0, 0, 0]
+WHITE = [255, 255, 255]
+
+# Access in code with `SPATHS[:my_sprite]`
+# Replace with your sprites!
+SPATHS = {
+  my_sprite: "sprites/my_sprite.png",
+}
+
+def debug?
+  !!$gtk.production
+end
+
+def tick(args)
+  args.outputs.background_color = TRUE_BLACK
+
+  debug_tick(args)
+end
+
+def debug_tick(args)
+  args.outputs.debug << [args.grid.w - 12, args.grid.h, "#{args.gtk.current_framerate.round}", 0, 1, *WHITE].label
+
+  if debug? && args.inputs.keyboard.key_up.i
+    SPATHS.each { |_, v| args.gtk.reset_sprite(v) }
+    args.gtk.notify!("Sprites reloaded")
+  end
+end
